93b8a27015b35e3c6fb497df54a60deef7c602e6
Donovan <triops124@gmail.com>
2019-04-19 18:54:01 -0400

    Update index.php

--- index.php
+++ index.php
@@ -10,14 +10,14 @@ $data[$tagid] = $tagvalue;
 $newjson = json_encode($data);   
 file_put_contents('tags.json', $newjson); 
 fclose($file);
-echo '<html><body><form action="index.php?tagid='.$tagid.'" method="post"><input type="text" name="tagvalue" value="'.$tagvalue.'"><br><input type="save"></form></body></html>';
+echo '<html><form action="index.php?tagid='.$tagid.'" method="post"><input type="text" name="tagvalue" value="'.$tagvalue.'"><br><input type="save"></form></html>';
 }
 else if(!isset($_POST["tagvalue"])) {
 $file = fopen("tags.json","a+") or die ("file not found"); 
 $json = file_get_contents('tags.json');
 $data = json_decode($json, true);
 @$output = $data["$tagid"];
-echo '<html><body><form action="index.php?tagid='.$tagid.'" method="post"><input type="text" name="tagvalue" value="'.$output.'"><br><input type="save"></form></body></html>';
+echo '<html><form action="index.php?tagid='.$tagid.'" method="post"><input type="text" name="tagvalue" value="'.$output.'"><br><input type="save"></form></html>';
 fclose($file);
 }
 ?>
